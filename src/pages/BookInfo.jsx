f0f4e2890f2a9bc799bd0245853535c0b5bbb68d
RyanCacicedo <Ryancacicedo@gmail.com>
2023-02-10 14:14:23 -0500

    Routing

--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -1,19 +1,31 @@
-import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const BookInfo = ({ books }) => {
-    return ( 
-        <div className="books__body">
-            <main id='books__main'>
-                <div className="books__container">
-                    <div className="row">
-                        <div className="book__selected--top">
-                            
-                        </div>
-                    </div>
-                </div>
-            </main>
+  return (
+    <div className="books__body">
+      <main id="books__main">
+        <div className="books__container">
+          <div className="row">
+            <div className="book__selected--top">
+              <Link to="/books" className="book__link">
+                <FontAwesomeIcon icon="arrow-left" />
+              </Link>
+              <Link to="/book" className="book__link">
+                <h2 className="book__selected--title--top">Books</h2>
+              </Link>
+            </div>
+            <div className="book__selected">
+                <figure className="book__selected--figure">
+                    <img src="https://covers.openlibrary.org/b/id/8091016-L.jpg" alt="" className="book__selected--img" />
+                </figure>
+            </div>
+          </div>
         </div>
-     );
-}
- 
+      </main>
+    </div>
+  );
+};
+
 export default BookInfo;
